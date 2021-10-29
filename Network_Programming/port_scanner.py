acf7a44eae46896fb04a7c41ebd6705af28ba053
artuguen28 <arthur.uguen28@gmail.com>
2021-10-29 15:05:57 -0400

    Port Scanner

---
+++ Network_Programming/port_scanner.py
@@ -0,0 +1,28 @@
+import socket
+from queue import Queue
+import threading
+
+target = "127.0.0.1"
+
+q = Queue()
+for x in range(1, 501):
+    q.put(x)
+
+def portscan(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        conn = s.connect((target, port))
+        return True
+    except:
+        return False
+
+def worker():
+    while True:
+        port = q.get()
+        if portscan(port):
+            print(f"Port {port} is open!")
+
+for x in range(30):
+    t = threading.Thread(target=worker)
+    t.start()

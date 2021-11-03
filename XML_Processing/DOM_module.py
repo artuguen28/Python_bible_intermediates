cc27f0c3c8b621b077b841b1fa45ede8b5fa73c6
artuguen28 <arthur.uguen28@gmail.com>
2021-11-03 08:42:25 -0400

    DOM and SAX module

---
+++ XML_Processing/DOM_module.py
@@ -0,0 +1,18 @@
+import xml.dom.minidom
+import os
+
+
+domtree = xml.dom.minidom.parse("XML_Processing\group.xml")
+group = domtree.documentElement
+
+persons = group.getElementsByTagName("person")
+
+for person in persons:
+    print("--- Persons ---")
+    if person.hasAttribute("id"):
+        print("ID: %s" % person.getAttribute("id"))
+
+    name = person.getElementsByTagName ("name")[0]
+    age = person.getElementsByTagName("age")[0]
+    weight = person.getElementsByTagName("weight")[0]
+    height = person.getElementsByTagName("height")[0]
